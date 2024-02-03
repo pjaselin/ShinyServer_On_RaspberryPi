8332ce55dd316e863deb279d53cf6ed1635ba54f
pjaselin <26674234+pjaselin@users.noreply.github.com>
2024-02-03 18:12:44 -0500

    added git install as dependency and collapsed commands into single lines

--- StableInstall_RPiShinyServer.sh
+++ StableInstall_RPiShinyServer.sh
@@ -6,22 +6,17 @@
 # - https://github.com/rstudio/shiny-server/wiki/Building-Shiny-Server-from-Source
 # - http://docs.rstudio.com/shiny-server/#systemd-redhat-7-ubuntu-15.04-sles-12
 # - https://www.raspberrypi-spy.co.uk/2015/10/how-to-autorun-a-python-script-on-boot-using-systemd/
+# - https://github.com/rstudio/shiny-server/wiki/Building-Shiny-Server-from-Source#what-if-a-sufficiently-recent-version-of-cmake-isnt-available
 
 # Start at home directory
 cd ~/
 
-# Update/Upgrade Raspberry Pi
-sudo apt-get -y update && sudo apt-get -y upgrade
-
-# Install R
-sudo apt-get -y install r-base
-
-# Install system libraries (dependences for some R packages)
-sudo apt-get -y install libssl-dev libcurl4-openssl-dev libboost-atomic-dev
-
-## Uninstall/Reinstall Pandoc (Shouldn't be initially installed but doing this to ensure we have a clean install)
+# Uninstall Pandoc (Shouldn't be initially installed but doing this to ensure we have a clean install)
 sudo apt-get -y remove pandoc
-sudo apt-get -y install pandoc
+
+# Update/Upgrade Raspberry Pi and install required apt packages
+sudo apt-get -y update \
+    && apt-get -y install r-base libssl-dev libcurl4-openssl-dev libboost-atomic-dev pandoc cmake git
 
 # Install R Packages
 sudo su - -c "R -e \"install.packages('httpuv', repos='https://cran.rstudio.com/')\""
@@ -29,12 +24,8 @@ sudo su - -c "R -e \"install.packages('shiny', repos='https://cran.rstudio.com/'
 sudo su - -c "R -e \"install.packages('plotly', repos='https://cran.rstudio.com/')\""
 sudo su - -c "R -e \"install.packages('rmarkdown', repos='https://cran.rstudio.com/')\""
 
-# Install cmake: https://github.com/rstudio/shiny-server/wiki/Building-Shiny-Server-from-Source#what-if-a-sufficiently-recent-version-of-cmake-isnt-available
-sudo apt-get -y install cmake
-sudo apt-get -y update && sudo apt-get -y upgrade
-
 # Clone the Shiny Server repository from GitHub in the tmp directory
-mkdir ~/tmp && cd tmp
+mkdir -p ~/tmp && cd tmp
 git clone https://github.com/rstudio/shiny-server.git && cd shiny-server
 
 # Because we are on a Raspberry Pi, we need to edit the external/node/install-node.sh file to use an ARM-compatible NodeJS installation (https://github.com/nodejs/node#verifying-binaries)
@@ -64,11 +55,8 @@ sudo ln -s /usr/local/shiny-server/bin/shiny-server /usr/bin/shiny-server
 sudo useradd -r -m shiny
 
 # Create log, config, and application directories
-sudo mkdir -p /var/log/shiny-server
-sudo mkdir -p /srv/shiny-server
-sudo mkdir -p /var/lib/shiny-server
+sudo mkdir -p /var/log/shiny-server /srv/shiny-server /var/lib/shiny-server /etc/shiny-server
 sudo chown shiny /var/log/shiny-server
-sudo mkdir -p /etc/shiny-server
 
 # Return to Shiny Server directory and set shiny-server.conf
 cd shiny-server
@@ -92,4 +80,4 @@ sudo cp -r samples/sample-apps/ /srv/shiny-server/
 sudo shiny-server &
 
 # Return to home directory
-cd
+cd ~/
